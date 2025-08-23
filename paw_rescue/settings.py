e1e780f41495dc58aedbc5b62c027011214d64fb
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-23 10:36:27 +0530

    Update settings.py remaked

--- paw_rescue/settings.py
+++ paw_rescue/settings.py
@@ -1,122 +1,134 @@
-import os
+"""
+Django settings for paw_rescue project.
+"""
+
 from pathlib import Path
+import os
 import dj_database_url
 
+# ------------------------
+# BASE DIRECTORY
+# ------------------------
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-
-ALLOWED_HOSTS = ["*"]  # change later for security
-
+# ------------------------
+# SECURITY
+# ------------------------
+SECRET_KEY = 'django-insecure-8f4t!$3&4qgpjoor-c7xp4sk+*jkp-7vf4%x35$ah1o!g$2a)f'
+DEBUG = True
+ALLOWED_HOSTS = ["*"]  # allow all hosts for Render deployment
 
 # ------------------------
 # INSTALLED APPS
 # ------------------------
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "cloudinary",
-    "cloudinary_storage",
-    # your apps here
+    # Django default apps
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Your apps
+    'accounts',
+    'feed',
+    'adminpanel',
+
+    # Cloudinary for media storage
+    'cloudinary',
+    'cloudinary_storage',
 ]
 
+# ------------------------
+# MIDDLEWARE
+# ------------------------
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # static files
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = "yourproject.urls"
+# ------------------------
+# URLS & TEMPLATES
+# ------------------------
+ROOT_URLCONF = 'paw_rescue.urls'
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / "templates"],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / "templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = "yourproject.wsgi.application"
-
-
-# ------------------------
-# DATABASE
-# ------------------------
-if os.environ.get("RENDER"):
-    # Production (Render + Neon)
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=True,
-        )
-    }
-else:
-    # Local Dev (SQLite)
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
-
+WSGI_APPLICATION = 'paw_rescue.wsgi.application'
 
 # ------------------------
-# PASSWORDS
+# DATABASE (PostgreSQL Neon)
 # ------------------------
-AUTH_PASSWORD_VALIDATORS = [
-    {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
-]
-
-
-# ------------------------
-# STATIC & MEDIA
-# ------------------------
-STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
-
-MEDIA_URL = "/media/"
-DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
-
-
-# ------------------------
-# CLOUDINARY CONFIG
-# ------------------------
-CLOUDINARY_STORAGE = {
-    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
-    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
-    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+DATABASES = {
+    "default": dj_database_url.parse(
+        "postgresql://neondb_owner:npg_bGguxX0VRQ7w@ep-green-brook-adn3o7bq-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
+# ------------------------
+# PASSWORD VALIDATION
+# ------------------------
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 # ------------------------
-# OTHER SETTINGS
+# INTERNATIONALIZATION
 # ------------------------
-LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# ------------------------
+# STATIC FILES
+# ------------------------
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# ------------------------
+# MEDIA FILES (Cloudinary)
+# ------------------------
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'dhol8imhb',
+    'API_KEY': '616112266455922',
+    'API_SECRET': 'LVW7RCMdSQzSFQHrP5di_K58p4w',
+}
+
+MEDIA_URL = '/media/'  # URL for media
+
+# ------------------------
+# DEFAULT PK & LOGIN
+# ------------------------
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/feed/'
