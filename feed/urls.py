a30fb5bf6826af6cc14d3e6ef3e55fc100174682
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-29 13:47:01 +0530

    x8

--- feed/urls.py
+++ feed/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
-app_name='feed'
+
+app_name = 'feed'
+
 urlpatterns = [
-    path('', views.feed, name='feed'),  # Feed page
+    path('', views.feed, name='feed'),
     path('report/', views.report, name='report'),
     path('adoptions/', views.adoption_public, name='adoption_public'),
     path('volunteer/', views.volunteer_form, name='volunteer_form'),
     path('food/', views.food_view, name="food"),
     path('health/', views.health, name="health"),
-    path('location/',views.location,name="location")
-
+    path('location/', views.location, name="location"),
 ]
