2868e957f69e2de9bc69706ff743b1578ebd4799
Vanshhsoni <sonivanshmaster@gmail.com>
2025-08-29 13:14:59 +0530

    x6

--- payment/urls.py
+++ payment/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name="payment"
 urlpatterns = [
     path('donate/', views.donate_page, name="donate_page"),
     path('payment-success/', views.payment_success, name="payment_success"),
